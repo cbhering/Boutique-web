a51afb8834b77e9d360038473e86124a251d1635
unknown <eleves@PC304-04.College_Andre-Grasset.local>
2020-09-20 20:08:47 -0400

    First commit

---
+++ Boutique_web/js/boutique.js
@@ -0,0 +1,140 @@
+﻿var tab_produits = [];
+var panier = [];
+
+window.onload = function () {
+    chargerProduits();
+
+
+};
+
+//Il s'agit de la fonction qui va charger les produits dans le catalogue du magasin en créant tout d'abord un tableau associatif (dictionnaire, dans d'autres langages).
+function chargerProduits() {
+    tab_produits["canon-t8i.jpg"] = 1199.00;
+    tab_produits["canon-90D.jpg"] = 1999.99;
+    tab_produits["nikon-z50.jpg"] = 1599.00;
+    tab_produits["flash-godox.jpg"] = 229.95;
+    tab_produits["objectif-canon.jpg"] = 669.00;
+    tab_produits["objectif-canon-macro.jpg"] = 2899.00;
+    tab_produits["trepied.jpg"] = 239.95
+    tab_produits["sac.jpg"] = 69.95;
+    tab_produits["carte-memoire.jpg"] = 52.95;
+
+}
+
+//Cette fonction va transférer/ajouter les produits à la table de la gauche pour que le total des produits soit calculé ensuite.
+function ajouterAuPanier(item) {
+    if ((itemExistant(item)) == false) {
+        panier[item] = tab_produits[item];
+
+        //À chaque création de l'élément html qui correspond à la table, on va créer des attributs applicables.
+        //Dans ce cas, on va créer les lignes (en anglais, row).
+        var ligneTable = document.createElement("tr");
+        ligneTable.setAttribute("id", item);
+        ligneTable.setAttribute("onClick", "retirerDuPanier()");
+        ligneTable.onclick = retirerDuPanier;
+
+        //Création des cellules liées à l'image et au prix.
+        //Ajout des cellules dans les lignes de chaque produit dans le panier.
+        var celluleImage = document.createElement("td");
+        var cellulePrix = document.createElement("td");
+        ligneTable.appendChild(celluleImage);
+        ligneTable.appendChild(cellulePrix);
+
+        //Création de l'émement html image et les attributs correspondants.
+        //Ajout de l'image dans la cellule de l'image.
+        var image = document.createElement("img");
+        image.setAttribute("src", "images/" + item);
+        image.setAttribute("width", "60px");
+        celluleImage.appendChild(image);
+
+        //Création de la variable du prix avec le format correcte et son ajout dans la cellule du prix.
+        var prixContenu = document.createTextNode(panier[item].toFixed(2) + " $");
+        cellulePrix.appendChild(prixContenu);
+
+        //Finalement, toute la ligne sera ajouter au corps (élément body) du panier.
+        document.getElementById("bodyPanier").appendChild(ligneTable);
+        afficherTotal();
+    }
+
+}
+
+//Fonction pour retirer chaque produit du panier.
+function retirerDuPanier() {
+    //This va récupérer l'objet/élément courant, qui est l'élément fils du node correspondant.
+    //Donc, removeChild va supprimer l'élément tr de tbody.
+    this.parentNode.removeChild(this);
+    delete panier[this.id];
+    afficherTotal();
+}
+
+//Fonction pour afficher le prix total après la somme de tous les produits.
+//Il y a aussi le formatage du prix après la virgule.
+function afficherTotal() {
+    var montant = 0.0;
+    //Boucle for pour ajouter les prix dans le panier de l'achat.
+    for (var prix in panier) {
+        montant += panier[prix];
+    }
+    document.getElementById("divTotalPanier").innerHTML = montant.toFixed(2)
+}
+
+/*  La fonction itemExistant valide si l'item est déjà dans le panier et
+ *  retourne true s'il est présent, et false s'il ne l'est pas  */
+function itemExistant(item) {
+    for (var indice in panier) {
+        if (indice == item) {
+            alert("Ce produit existe déjà dans le panier");
+            return true;
+        }
+    }
+    return false;
+
+}
+
+//Il s'agit de la fonction qui sert à vérifier toutes les entrées par l'utilisateur dans les champs du formulaire de paiment.
+//On a crée des variables booléenes pour chaque vérification afin de les récupérer plus tard et valider si toutes les données de l'utilisateur sont correctes.
+function validerPaiement() {
+
+    var titulaire = false;
+    var titulaireNom = document.getElementById("nom").value.trim();
+    if (titulaireNom.length < 3) {
+        alert("Le nom doit être plus long que 3 caractères.");
+    } else {
+        titulaire = true;
+    }
+
+    var carte = false;
+    var carteNumero = document.getElementById("numero").value.trim();
+    if ((carteNumero.charAt(0) != 4) && (carteNumero.charAt(0) != 5)) {
+        alert("Seulement les cartes Visa et MasterCard sont acceptées.");
+    } else {
+        carte = true;
+    }
+
+    var longueurCarte = false;
+    if (carteNumero.length != 16) {
+        alert("La carte de crédit doit avoir exactement 16 chiffres.");
+    } else {
+        longueurCarte = true;
+    }
+
+    var numeroCvc = false;
+    var cvcNumero = document.getElementById("cvc").value.trim();
+    if (!/^[0-9]+$/.test(cvcNumero)) {
+        alert("Le code CVC doit être des chiffres");
+    } else if (cvcNumero.length != 3) {
+        alert("Le code CVC doit avoir exactement 3 chiffres");
+    } else {
+        numeroCvc = true;
+    }
+
+    var dateFormatage = { weekday: 'long', year: 'numeric', month: 'long', day: '2-digit' };
+    var date = new Date();
+    var heure = new Date();
+    var formatageHoraire = { hour: '2-digit', minute: 'numeric' }
+    if (titulaire == true && carte == true && longueurCarte == true && numeroCvc == true) {
+        alert("Paiement effectué avec succès le " + date.toLocaleString("fr-CA", dateFormatage) + " à " + heure.toLocaleString("fr-CA", formatageHoraire).replace('h', ':').replace(/ /g, ''));
+    }
+    return true;
+
+}
---
+++ Boutique_web/js/repertoire.js
@@ -0,0 +1,155 @@
+﻿var tab_photographes = []
+
+function Photographe(nom, region, type) {
+    this.nom = nom;
+    this.region = region;
+    this.type = type;
+}
+
+window.onload=function() {
+  chargerPhotographes();
+  document.getElementById("rafraichir").addEventListener("click", rafraichir);
+
+};
+
+//Création des objets liés aux photographes pour que leurs informations soient affichés lors du lancement de la page.
+function chargerPhotographes() {
+    var annie = new Photographe("Annie Simard", "Montréal", ["Évenement", "Corporatif"]);
+    tab_photographes.push(annie);
+    var joey = new Photographe("Joey Michaud", "Sherbrooke", ["Corporatif"]);
+    tab_photographes.push(joey);
+    var micheal = new Photographe("Micheal Smith", "Québec", ["Famille"]);
+    tab_photographes.push(micheal);
+    var steph = new Photographe("Stéphanie Dulude", "Québec", ["Mariage"]);
+    tab_photographes.push(steph);
+    var isabelle = new Photographe("Isabelle Thériault", "Québec", ["Famille", "Mariage"]);
+    tab_photographes.push(isabelle);
+    var magalie = new Photographe("Magalie Simoneau", "Montréal", ["Mariage"]);
+    tab_photographes.push(magalie);
+    var john = new Photographe("John Harvey", "Trois-Rivière", ["Corporatif"]);
+    tab_photographes.push(john);
+
+    afficherPhotographes(tab_photographes);
+
+}
+
+//Fonction qui va effectivement afficher les photographes lors du lancement de la page.
+function afficherPhotographes(tableau) { 
+    var affichageTable = "";
+
+    tableau.forEach(function (photographe) {
+        affichageTable += "<tr><td>" + photographe.nom + "</td><td>" + photographe.region + "</td><td>" + photographe.type.join(" - ") + "</td></tr>";
+    })
+
+    document.getElementById("tableau-photographes").innerHTML = affichageTable;
+
+}
+
+//Cette fonction travaille avec le filtre de type en récupérant l'option choisi par l'utilisateur dans l'id filtrer-type avec l'événement onClick.
+//Par la suite, nous allons utiliser la technique de vider le tableau tab_photographes, créer seulement les objets qui correspondent au choix de l'utilisateur
+//et les ajouter dans le tableau tab_photographes. Après ça, on va inséerer les lignes dans le tbody de la table dont l'id est tableau-photographes.
+function filtrerParType(e) {
+  
+    let choixType = document.getElementById("filtrer-type").options[document.getElementById("filtrer-type").selectedIndex].text;
+
+    if (choixType == "Famille") {
+        tab_photographes = [];
+        var micheal = new Photographe("Micheal Smith", "Québec", ["Famille"]);
+        tab_photographes.push(micheal);
+        var isabelle = new Photographe("Isabelle Thériault", "Québec", ["Famille", "Mariage"]);
+        tab_photographes.push(isabelle);
+        document.getElementById("tableau-photographes").innerHTML = "<tr><td>" + micheal.nom + "</td><td>" + micheal.region + "</td><td>" + micheal.type.join(" - ") + "</td></tr>"
+            + "<tr><td>" + isabelle.nom + "</td><td>" + isabelle.region + "</td><td>" + isabelle.type.join(" - ") + "</td></tr>";
+    } else if (choixType == "Évenement") {
+        tab_photographes = [];
+        var annie = new Photographe("Annie Simard", "Montréal", ["Évenement", "Corporatif"]);
+        tab_photographes.push(annie);
+        document.getElementById("tableau-photographes").innerHTML = "<tr><td>" + annie.nom + "</td><td>" + annie.region + "</td><td>" + annie.type.join(" - ") + "</td></tr>";
+    } else if (choixType == "Corporatif") {
+        tab_photographes = [];
+        var annie = new Photographe("Annie Simard", "Montréal", ["Évenement", "Corporatif"]);
+        tab_photographes.push(annie);
+        var joey = new Photographe("Joey Michaud", "Sherbrooke", ["Corporatif"]);
+        tab_photographes.push(joey);
+        var john = new Photographe("John Harvey", "Trois-Rivière", ["Corporatif"]);
+        tab_photographes.push(john);
+        document.getElementById("tableau-photographes").innerHTML = "<tr><td>" + annie.nom + "</td><td>" + annie.region + "</td><td>" + annie.type.join(" - ") + "</td></tr>"
+            + "<tr><td>" + joey.nom + "</td><td>" + joey.region + "</td><td>" + joey.type.join(" - ") + "</td></tr>"
+            + "<tr><td>" + john.nom + "</td><td>" + john.region + "</td><td>" + john.type.join(" - ") + "</td></tr>";
+    } else {
+        tab_photographes = [];
+        var steph = new Photographe("Stéphanie Dulude", "Québec", ["Mariage"]);
+        tab_photographes.push(steph);
+        var isabelle = new Photographe("Isabelle Thériault", "Québec", ["Famille", "Mariage"]);
+        tab_photographes.push(isabelle);
+        var magalie = new Photographe("Magalie Simoneau", "Montréal", ["Mariage"]);
+        tab_photographes.push(magalie);
+        document.getElementById("tableau-photographes").innerHTML = "<tr><td>" + steph.nom + "</td><td>" + steph.region + "</td><td>" + steph.type.join(" - ") + "</td></tr>"
+            + "<tr><td>" + isabelle.nom + "</td><td>" + isabelle.region + "</td><td>" + isabelle.type.join(" - ") + "</td></tr>"
+            + "<tr><td>" + magalie.nom + "</td><td>" + magalie.region + "</td><td>" + magalie.type.join(" - ") + "</td></tr>";
+    }
+}
+
+//Cette fonction travaille avec le filtre de region en récupérant l'option choisi par l'utilisateur dans l'id filtrer-region avec l'événement onClick.
+//Par la suite, nous allons utiliser la technique de vider le tableau tab_photographes, créer seulement les objets qui correspondent au choix de l'utilisateur
+//et les ajouter dans le tableau tab_photographes. Après ça, on va inséerer les lignes dans le tbody de la table dont l'id est tableau-photographes.
+function filtrerParRegion(e){
+
+    let choixRegion = document.getElementById("filtrer-region").options[document.getElementById("filtrer-region").selectedIndex].text;
+
+    if (choixRegion == "Montréal") {
+        tab_photographes = [];
+        var annie = new Photographe("Annie Simard", "Montréal", ["Évenement", "Corporatif"]);
+        tab_photographes.push(annie);
+        var magalie = new Photographe("Magalie Simoneau", "Montréal", ["Mariage"]);
+        tab_photographes.push(magalie);
+        document.getElementById("tableau-photographes").innerHTML = "<tr><td>" + annie.nom + "</td><td>" + annie.region + "</td><td>" + annie.type.join(" - ") + "</td></tr>"
+            + "<tr><td>" + magalie.nom + "</td><td>" + magalie.region + "</td><td>" + magalie.type.join(" - ") + "</td></tr>";
+    } else if (choixRegion == "Québec") {
+        tab_photographes = [];
+        var micheal = new Photographe("Micheal Smith", "Québec", ["Famille"]);
+        tab_photographes.push(micheal);
+        var steph = new Photographe("Stéphanie Dulude", "Québec", ["Mariage"]);
+        tab_photographes.push(steph);
+        var isabelle = new Photographe("Isabelle Thériault", "Québec", ["Famille", "Mariage"]);
+        tab_photographes.push(isabelle);
+        document.getElementById("tableau-photographes").innerHTML = "<tr><td>" + micheal.nom + "</td><td>" + micheal.region + "</td><td>" + micheal.type.join(" - ") + "</td></tr>"
+            + "<tr><td>" + steph.nom + "</td><td>" + steph.region + "</td><td>" + steph.type.join(" - ") + "</td></tr>"
+            + "<tr><td>" + isabelle.nom + "</td><td>" + isabelle.region + "</td><td>" + isabelle.type.join(" - ") + "</td></tr>";
+    } else if (choixRegion == "Trois-Rivière") {
+        tab_photographes = [];
+        var john = new Photographe("John Harvey", "Trois-Rivière", ["Corporatif"]);
+        tab_photographes.push(john);
+        document.getElementById("tableau-photographes").innerHTML = "<tr><td>" + john.nom + "</td><td>" + john.region + "</td><td>" + john.type.join(" - ") + "</td></tr>";
+    } else {
+        tab_photographes = [];
+        var joey = new Photographe("Joey Michaud", "Sherbrooke", ["Corporatif"]);
+        tab_photographes.push(joey);
+        document.getElementById("tableau-photographes").innerHTML = "<tr><td>" + joey.nom + "</td><td>" + joey.region + "</td><td>" + joey.type.join(" - ") + "</td></tr>";
+    }
+}
+
+//Il s'agit de la fonction qui sera déclenchée à partir de l'événement onkeyup.
+function filtrerParNom(e){
+
+    var texteValeur;
+    var saisie = document.getElementById("nom");
+    var filtre = saisie.value.toUpperCase();
+    var table = document.getElementById("tableau-photographes");
+    var ligne = table.getElementsByTagName("tr");
+    for (var cpt = 0; cpt < ligne.length; cpt++) {
+        cellule = ligne[cpt].getElementsByTagName("td")[0];
+        if (cellule) {
+            texteValeur = cellule.textContent || cellule.innerText;
+            if (texteValeur.toUpperCase().indexOf(filtre) > -1) {
+                ligne[cpt].style.display = "";
+            } else {
+                ligne[cpt].style.display = "none";
+            }
+        }
+    }
+}
+
+function rafraichir(){
+  location.reload(); 
+}
